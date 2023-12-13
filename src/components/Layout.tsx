7a55cc77d924d67bc1c7d7d86e9c7afa7752bfce
Brandon Trzepacz Tish <46979741+mackentish@users.noreply.github.com>
2023-12-13 14:35:29 -0500

    (WIP)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Menu } from ".";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <main className="flex flex-row min-h-screen min-w-screen">
+      <Menu />
+      <div className="flex flex-row justify-center items-center w-full">
+        {children}
+      </div>
+    </main>
   );
 }
