e2ceb0da50f071618f6f91d538b4bbf71744f3ed
Brandon Trzepacz Tish <46979741+mackentish@users.noreply.github.com>
2023-12-14 09:29:50 -0500

    remove Bars

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { ListItem as ListItemType } from "../types";
-import { OpenTab, Bars } from "./icons";
+import { OpenTab } from "./icons";
 import { Checkbox } from ".";
 
 interface ListItemProps {
@@ -29,11 +29,7 @@ export function ListItem({ item, isOwner }: ListItemProps) {
       className="flex flex-row items-center justify-between p-2 border-2 border-gray-100 rounded-md"
     >
       <div className="flex flex-row items-center gap-2">
-        {isOwner ? (
-          <Bars />
-        ) : (
-          <Checkbox checked={item.isBought} onClick={handleClick} />
-        )}
+        {!isOwner && <Checkbox checked={item.isBought} onClick={handleClick} />}
         <div className="flex flex-col">
           <p>{item.name}</p>
           {item.note && <p className="text-sm text-gray-500">{item.note}</p>}
