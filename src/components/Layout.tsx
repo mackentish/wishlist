fd32d76afb4df4ffa93636d3495f4913f423af61
Brandon Trzepacz Tish <46979741+mackentish@users.noreply.github.com>
2023-12-19 13:21:47 -0500

    fix hydration issue

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,20 +1,13 @@
 import React from "react";
 import { Menu } from ".";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <main className="flex flex-row min-h-screen min-w-screen">
-          <Menu />
-          <div className="flex flex-row justify-center items-center w-full">
-            {children}
-          </div>
-        </main>
-      </body>
-    </html>
+    <main className="flex flex-row min-h-screen min-w-screen">
+      <Menu />
+      <div className="flex flex-row justify-center items-center w-full">
+        {children}
+      </div>
+    </main>
   );
 }
