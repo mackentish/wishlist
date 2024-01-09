816a53f49df3c1cdbdf2a114a2e8dc5a34902609
Brandon Trzepacz Tish <46979741+mackentish@users.noreply.github.com>
2024-01-09 15:32:25 -0500

    add (but not use) custom color theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,22 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        white: "#FEFCF2",
+        black: "#252422",
+        lightGrey: "#CCC5B9",
+        darkGrey: "#3F3D39",
+        primary: "#EB5E27",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
