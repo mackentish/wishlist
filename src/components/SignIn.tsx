3a69ce837a144a49cddbeec2eb312e65fd4df1b0
Brandon Trzepacz Tish <46979741+mackentish@users.noreply.github.com>
2024-01-17 14:37:14 -0500

    more tests

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -12,6 +12,16 @@ export function SignIn() {
         Not signed in
       </p>
       <Button onClick={() => signIn()}>Sign in</Button>
+      <Button
+        onClick={async () => {
+          const endpoint = "/data-api/rest/user";
+          const response = await fetch(endpoint);
+          const data = await response.json();
+          console.table(data.value);
+        }}
+      >
+        TEST
+      </Button>
     </div>
   );
 }
