fd31b3131a1079944feee1aea1cdfdeddc1e99e1
Brandon Trzepacz Tish <46979741+mackentish@users.noreply.github.com>
2024-01-23 14:59:48 -0500

    remove linkExpires column

---
+++ prisma/migrations/20240123195920_remove_link_expires_column/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `linkExpires` on the `list` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "list" DROP COLUMN "linkExpires";
