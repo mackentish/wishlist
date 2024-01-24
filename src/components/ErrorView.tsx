63a58e2f3848f79d2e35a610e4e89de81265aaaf
Brandon Trzepacz Tish <46979741+mackentish@users.noreply.github.com>
2024-01-24 09:50:18 -0500

    remove punctuation before emoji

--- src/components/ErrorView.tsx
+++ src/components/ErrorView.tsx
@@ -4,7 +4,7 @@ export function ErrorView() {
   return (
     <div className="flex flex-col gap-8 items-center w-full max-w-3xl">
       <p className="font-mono text-xl text-black dark:text-white">
-        Something went wrong. Please try again later. 😞
+        Something went wrong. Please try again later 😞
       </p>
     </div>
   );
