417ceea235135e258dc73316b3d26ba15cda8917
Brandon Trzepacz Tish <brandon.tish@gmail.com>
2024-03-27 10:03:36 -0400

    add Header component to git

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export function Header() {
+    return (
+        <header className="flex flex-row gap-2 items-baseline justify-center w-full p-8">
+            <h1 className="font-bold text-5xl text-black dark:text-white">
+                wishlist
+            </h1>
+            <p className="text-xs text-darkGrey dark:text-lightGrey ">
+                by @mackentish
+            </p>
+        </header>
+    );
+}
