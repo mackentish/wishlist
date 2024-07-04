df9bbe546cedaa45885c5a015296a4dafaeca454
Brandon Trzepacz Tish <brandon.tish@gmail.com>
2024-07-04 18:02:06 -0400

    update sign in to have disabled button while signing in

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,13 +1,22 @@
 import { Button, Header } from '@/components';
 import { signIn } from 'next-auth/react';
-import React from 'react';
+import React, { useState } from 'react';
 
 export function SignIn() {
+    const [isLoading, setIsLoading] = useState(false);
+
+    const signUserIn = () => {
+        setIsLoading(true);
+        signIn('google');
+    };
+
     return (
         <div className="flex flex-col gap-8 items-center w-full max-w-3xl">
             <Header />
             <p className="text-xl text-black dark:text-white">Not signed in</p>
-            <Button onClick={() => signIn('google')}>Sign in</Button>
+            <Button disabled={isLoading} onClick={signUserIn}>
+                Sign in
+            </Button>
         </div>
     );
 }
